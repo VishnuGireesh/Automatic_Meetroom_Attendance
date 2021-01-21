a7f7798793891013c49c5309eac04de398b22767
gopikagopan123 <57207860+gopikagopan123@users.noreply.github.com>
2021-01-21 21:38:59 +0530

    Create trainmodel.py

---
+++ trainmodel.py
@@ -0,0 +1,29 @@
+# Automatic_Meetroom_Attendance
+import numpy as np
+import os
+import face_recognition as fr
+
+
+def get_encoded_faces():
+
+    encoded = {}
+
+    for dirpath, dname, fname in os.walk("D:\FaceAppTests\Automatic_Meetroom_Attendance\Faces"):
+        for f in fname:
+            i = 1
+            if f.endswith(".jpg") or f.endswith(".png"):
+                loc = os.path.join(dirpath, f)
+                face = fr.load_image_file(loc)
+                array = loc.split("\\")
+
+                if array[-2] in encoded.keys():
+                    array[-2] = array[-2] + str(i)
+                    i = i + 1
+
+                encoding = fr.face_encodings(face)[0]
+                encoded[array[-2]] = encoding
+
+                os.chdir("D:\FaceAppTests\Automatic_Meetroom_Attendance")
+                np.save("training_data.npy", encoded)
+
+    print("##........TRAINING COMPLETED...........##")
